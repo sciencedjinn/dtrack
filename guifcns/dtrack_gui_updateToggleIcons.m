a2930b919aad9524bca51eeb7a75bd0fc013de6c
Jochen Smolka <smolkajochen@gmail.com>
2020-02-05 16:23:09 +0100

    comments

--- guifcns/dtrack_gui_updateToggleIcons.m
+++ guifcns/dtrack_gui_updateToggleIcons.m
@@ -1,8 +1,19 @@
- function dtrack_gui_updateToggleIcons(cbo, gui) 
+function dtrack_gui_updateToggleIcons(cbo, gui) 
+% DTRACK_GUI_UPDATETOGGLEICONS reverses and brightens the cdata of a 
+% uicontrol toggle object, if the value is 1
+%
+% cbo - callback object handle, needs to be a uicontrol toggle object
+% gui - dtrack gui stucture. Needs to contain cdata in a field gui.icons.(tag),
+% where tag equals the 'tag' of the uicontrol object
+ 
     tag = get(cbo, 'tag');
+    icon = gui.icons.(tag);
+    
     if get(cbo, 'value')
-        set(cbo, 'cdata', 380-gui.icons.(tag));
+        rev_icon = 380-icon;
+        rev_icon(rev_icon>255) = 255;
+        set(cbo, 'cdata', rev_icon);
     else
-        set(cbo, 'cdata', gui.icons.(tag));
+        set(cbo, 'cdata', icon);
     end
 end
--- guifcns/dtrack_gui_updateTogglegroupIcons.m
+++ guifcns/dtrack_gui_updateTogglegroupIcons.m
@@ -1,12 +1,23 @@
 function dtrack_gui_updateTogglegroupIcons(cbo, gui)
+% DTRACK_GUI_UPDATETOGGLEGROUPICONS reverses and brightens the cdata of all 
+% uicontrol toggle objects in a uibuttongroup, if the value is 1
+ %
+ % cbo - callback object handle, needs to be a uicontrol toggle object
+ % whose parent is a uibuttongroup
+ % gui - dtrack gui stucture. Needs to contain cdata in a field gui.icons.(tag),
+ % where tag equals the 'tag' of each uicontrol object
+ 
     bg = get(cbo, 'parent');
     buttons = get(bg, 'children');
     for button = buttons(:)'
         tag = get(button, 'tag');
+        icon = gui.icons.(tag);
         if button == get(bg, 'SelectedObject')
-            set(button, 'cdata', 380-gui.icons.(tag));
+            rev_icon = 380-icon;
+            rev_icon(rev_icon>255) = 255;
+            set(button, 'cdata', rev_icon);
         else
-            set(button, 'cdata', gui.icons.(tag));
+            set(button, 'cdata', icon);
         end
     end
 end
